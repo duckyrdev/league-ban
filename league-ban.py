d40dd20b0a99af0a2befa78e219bb9ecf92d98e7
nngngn <79119564+nngngn@users.noreply.github.com>
2023-02-16 16:38:26 -0800

    Update league-ban.py

--- league-ban.py
+++ league-ban.py
@@ -4,5 +4,7 @@ client = discord.Client()
 
 @client.event
 async def on_member_update(before, after):
+    # Check if the member's activity is playing League of Legends
     if str(after.activity) == "League of Legends":
+        # Add the "Muted" role to the member
         await after.ban(reason="Playing League of Legends")
