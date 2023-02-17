1d04a77991d2fbf40437ff4037c0dae78069f3c0
nngngn <79119564+nngngn@users.noreply.github.com>
2023-02-16 16:39:17 -0800

    Update league-mute.py

--- league-mute.py
+++ league-mute.py
@@ -6,10 +6,10 @@ client = discord.Client()
 async def on_member_update(before, after):
     # Check if the member's activity is playing League of Legends
     if str(after.activity) == "League of Legends":
-        # Get the "Muted" role
-        muted_role = discord.utils.get(after.guild.roles, name="Muted")
+        # get "muted" role
+        muted_role = discord.utils.get(after.guild.roles, name="muted")
         
-        # Add the "Muted" role to the member
+        # add "muted" role to the member
         await after.add_roles(muted_role, reason="Playing League of Legends")
 
         
